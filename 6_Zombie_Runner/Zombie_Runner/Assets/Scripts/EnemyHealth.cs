e52754bf4743d4e31e0622c68d639ff2ee4a7850
Justin <justincoutts@icloud.com>
2021-02-24 09:54:04 +0000

    ZR_177_Enemy_Health_&_Damage

---
+++ 6_Zombie_Runner/Zombie_Runner/Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] float hitPoints = 100f;
+
+    //creat a public method which reduces hitpooints by the amount of damage
+
+
+    public void TakeDamage(float damage)
+    {
+        hitPoints -= damage;
+        if(hitPoints <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+    
+
+}
