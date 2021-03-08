b55b7abd4d270c193b2bec3369180278836b4fe3
Justin <justincoutts@icloud.com>
2021-03-08 10:33:56 +0000

    ZR_Weapon_Zoom_Mouse_Sensitiviy

--- 6_Zombie_Runner/Zombie_Runner/Assets/Scripts/WeaponsZoom.cs
+++ 6_Zombie_Runner/Zombie_Runner/Assets/Scripts/WeaponsZoom.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class WeaponsZoom : MonoBehaviour
 {
@@ -8,8 +9,17 @@ public class WeaponsZoom : MonoBehaviour
     [SerializeField] Camera fpsCamera;
     [SerializeField] float zoomOutFOV = 60f;
     [SerializeField] float zoomInFOV = 20f;
+    [SerializeField] float zoomOutSensitivity = 2f;
+    [SerializeField] float zoomInSensitivity = 0.5f;
+
+    RigidbodyFirstPersonController fpsController;
 
     bool zoomedInToggle = false;
+
+    private void Start()
+    {
+        fpsController = GetComponent<RigidbodyFirstPersonController>();
+    }
 
     private void Update()
     {
@@ -19,11 +29,15 @@ public class WeaponsZoom : MonoBehaviour
             {
                 zoomedInToggle = true;
                 fpsCamera.fieldOfView = zoomInFOV;
+                fpsController.mouseLook.XSensitivity = zoomInSensitivity;
+                fpsController.mouseLook.YSensitivity = zoomInSensitivity;
             }
             else
             {
                 zoomedInToggle = false;
                 fpsCamera.fieldOfView = zoomOutFOV;
+                fpsController.mouseLook.XSensitivity = zoomOutSensitivity;
+                fpsController.mouseLook.YSensitivity = zoomOutSensitivity;
             }
         }
     }
