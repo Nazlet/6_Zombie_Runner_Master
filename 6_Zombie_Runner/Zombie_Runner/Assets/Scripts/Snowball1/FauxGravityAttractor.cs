3c6879e69290613e860b65edb67258d48bc48d7a
Justin <justincoutts@icloud.com>
2021-07-12 12:34:15 +0100

    SB_20_Second_Attractor_Method_1

---
+++ 6_Zombie_Runner/Zombie_Runner/Assets/Scripts/Snowball1/FauxGravityAttractor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FauxGravityAttractor : MonoBehaviour
+{
+    public float gravity = -10f;
+    public Rigidbody rb1;
+
+    public void Attract(Transform body)
+    {
+        Vector3 gravityUp = (body.position - transform.position).normalized;
+        Vector3 bodyUp = body.up;
+
+        rb1.AddForce(gravityUp * gravity);
+
+        Quaternion targetRotation = Quaternion.FromToRotation(bodyUp, gravityUp) * body.rotation;
+        body.rotation = Quaternion.Slerp(body.rotation, targetRotation, 50 * Time.deltaTime);
+        
+    }
+}
---
+++ 6_Zombie_Runner/Zombie_Runner/Assets/Scripts/Snowball1/FauxGravityBody.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FauxGravityBody : MonoBehaviour
+{
+    public FauxGravityAttractor attractor;
+    private Transform myTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myTransform = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        attractor.Attract(myTransform);
+    }
+}
