ccee2799a3be9cf0d7221466c1e09719b28213a2
Justin <justincoutts@icloud.com>
2021-07-26 14:07:00 +0100

    SB_30_Harvester_Pathfinding

---
+++ 6_Zombie_Runner/Zombie_Runner/Assets/Scripts/Snowball1/HarvesterAI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class HarvesterAI : MonoBehaviour
+{
+    [SerializeField] Transform collector;
+
+    NavMeshAgent navMeshAgent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        navMeshAgent.SetDestination(collector.position);
+    }
+}
---
+++ 6_Zombie_Runner/Zombie_Runner/Assets/Scripts/Snowball1/HarvesterSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HarvesterSpawner : MonoBehaviour
+{
+    [Range(0.1f, 120f)]
+    [SerializeField] float secondsBetweenSpawns = 2f;
+    [SerializeField] GameObject harvestPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(RepeatedlySpawnHarvester());
+    }
+
+    IEnumerator RepeatedlySpawnHarvester()
+    {
+        while (true) //forever
+        {
+            Instantiate(harvestPrefab, transform.position, Quaternion.identity);
+            yield return new WaitForSeconds(secondsBetweenSpawns);
+        }
+    }
+
+}
