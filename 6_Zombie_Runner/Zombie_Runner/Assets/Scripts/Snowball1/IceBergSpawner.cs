32f944ee5f35d6670e3434b390311e3879e5b708
Justin <justincoutts@icloud.com>
2021-08-03 09:59:57 +0100

    SB_34_IceBergSpawner_Oscillator

---
+++ 6_Zombie_Runner/Zombie_Runner/Assets/Scripts/Snowball1/IceBergSpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IceBergSpawner : MonoBehaviour
+{
+    [Range(0.1f, 120f)]
+    [SerializeField] float secondsBetweenSpawns = 2f;
+    [SerializeField] GameObject icebergPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(RepeatedlySpawnIceberg());
+    }
+
+    IEnumerator RepeatedlySpawnIceberg()
+    {
+        while (true) //forever
+        {
+            Instantiate(icebergPrefab, transform.position, Quaternion.identity);
+            yield return new WaitForSeconds(secondsBetweenSpawns);
+        }
+    }
+}
